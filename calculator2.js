fd7445d08bd0336257825a6b676a3cadec48f231
Luizagalf <luizagalf@gmail.com>
2021-05-13 21:51:10 +0300

    Homework

--- calculator2.js
+++ calculator2.js
@@ -9,45 +9,53 @@ let n1 = parseFloat(firstNumber);
         document.getElementById('result').value = error;
         return;
     }
-let sum = +n1 + +n2;
-document.getElementById('result').value = sum;
+    let sum = +n1 + +n2;
+    document.getElementById('result').value = sum;
+    document.getElementById('firstnumber').value = "";
+    document.getElementById('secondnumber').value = "";
 }
 
 let subtraction = () => {
     let firstNumber = document.getElementById("firstnumber").value;
     let secondNumber = document.getElementById("secondnumber").value;
     let n1 = parseFloat(firstNumber);
-        let n2 = parseFloat(secondNumber);
+    let n2 = parseFloat(secondNumber);
         if (Number.isNaN(n1) || Number.isNaN(n2)) {
             document.getElementById('result').value = error;
             return;
         }
     let sub = n1 - n2;
     document.getElementById('result').value = sub;
+    document.getElementById('firstnumber').value = "";
+    document.getElementById('secondnumber').value = "";
     }
 
 let multiplication = () => {
-        let firstNumber = document.getElementById("firstnumber").value;
-        let secondNumber = document.getElementById("secondnumber").value;
-        let n1 = parseFloat(firstNumber);
-            let n2 = parseFloat(secondNumber);
-            if (Number.isNaN(n1) || Number.isNaN(n2)) {
-                document.getElementById('result').value = error;
-                return;
-            }
-        let mul = n1 * n2;
-        document.getElementById('result').value = mul;
+    let firstNumber = document.getElementById("firstnumber").value;
+    let secondNumber = document.getElementById("secondnumber").value;
+    let n1 = parseFloat(firstNumber);
+    let n2 = parseFloat(secondNumber);
+        if (Number.isNaN(n1) || Number.isNaN(n2)) {
+            document.getElementById('result').value = error;
+            return;
         }
+    let mul = n1 * n2;
+    document.getElementById('result').value = mul;
+    document.getElementById('firstnumber').value = "";
+    document.getElementById('secondnumber').value = "";
+    }
 
 let division = () => {
-            let firstNumber = document.getElementById("firstnumber").value;
-            let secondNumber = document.getElementById("secondnumber").value;
-            let n1 = parseFloat(firstNumber);
-                let n2 = parseFloat(secondNumber);
-                if (Number.isNaN(n1) || Number.isNaN(n2)) {
-                    document.getElementById('result').value = error;
-                    return;
-                }
-            let div = n1 / n2;
-            document.getElementById('result').value = div;
-            }
+    let firstNumber = document.getElementById("firstnumber").value;
+    let secondNumber = document.getElementById("secondnumber").value;
+    let n1 = parseFloat(firstNumber);
+    let n2 = parseFloat(secondNumber);
+        if (Number.isNaN(n1) || Number.isNaN(n2)) {
+            document.getElementById('result').value = error;
+            return;
+        }
+    let div = n1 / n2;
+    document.getElementById('result').value = div;
+    document.getElementById('firstnumber').value = "";
+    document.getElementById('secondnumber').value = "";
+    }
---
+++ div.js
@@ -0,0 +1,9 @@
+function changeAustria() {
+    let austria = document.getElementById ('austria');
+    austria.src = "https://www.estateservice.ru/files/images/v_avstrya.jpg";
+}
+
+function changeSwitzerland() {
+    let switzerland = document.getElementById ('switzerland');
+    switzerland.src = "https://micedata.s3.eu-central-1.amazonaws.com/MiceProjectData/Images/2020/3/16/135_337_e396ee56-a50f-4800-b83b-5a0f12355318.jpg";
+}
